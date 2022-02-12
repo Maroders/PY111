b945cb36db876a97473d99d86e1d3713f4fb42dc
Maroders <DShishlov@gmail.com>
2022-02-12 13:52:40 +0300

    a2 done_прошу проверить

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -8,15 +8,29 @@ from typing import Any
 
 class PriorityQueue:
     def __init__(self):
-        ...  # todo для очереди можно использовать python dict
+        self.priority_queue = {
+            0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: []
+        }  # todo для очереди можно использовать python dict
+        print(len(self.priority_queue)) # печатаю длину словаря
+        print(self.priority_queue) # печатаю словарь
 
     def enqueue(self, elem: Any, priority: int = 0) -> None:
         """
         Operation that add element to the end of the queue
 
+        :param priority:
         :param elem: element to be added
         :return: Nothing
         """
+        print(len(self.priority_queue)) # печатаю длину словаря
+        print(priority) # печатаю переданное значение приоритета
+        if priority > len(self.priority_queue) - 1:
+            # return None
+            raise IndexError("Too much") # предлагаю возвращать не None, а ошибку
+
+        self.priority_queue[priority].append(elem)
+        print(self.priority_queue) # печатаю словарь
+        print(self.priority_queue[priority]) # печатаю очередь из словаря согласно переданному приоритету
         return None
 
     def dequeue(self) -> Any:
@@ -25,16 +39,36 @@ class PriorityQueue:
 
         :return: dequeued element
         """
-        return None
+        print("++++++++++")
+        print(self.priority_queue) # поскольку при проверке в этой функции словарь, а следовательно и его длина обнуляются
+        # печатаю сам словарь ...
+        print("++++++++++")
+        len_ = len(self.priority_queue)
+        print("---------")
+        print(len_) # ... и его длину
+        print("---------")
+        print(self.priority_queue) # еще раз печатаю словарь, чтобы проверить, что все на месте
+        if not self.priority_queue:
+            return None
+
+        for i in range(len_):
+            if self.priority_queue[i]:
+                value = self.priority_queue[i][0]
+                del self.priority_queue[i][0]
+                print(self.priority_queue) # печатаю словарь
+                print(value) # печатаю значение элемента, который уходит из очереди
+                return value
 
     def peek(self, ind: int = 0, priority: int = 0) -> Any:
         """
         Allow you to see at the element in the queue without dequeuing it
 
+        :param priority:
         :param ind: index of element (count from the beginning)
         :return: peeked element
         """
-        return None
+        value = self.priority_queue[priority][ind]
+        return value
 
     def clear(self) -> None:
         """
@@ -42,4 +76,10 @@ class PriorityQueue:
 
         :return: None
         """
+        self.priority_queue.clear()
         return None
+
+
+# q = PriorityQueue()
+# q.enqueue(3)
+# q.dequeue()
