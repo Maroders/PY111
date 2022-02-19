da03746f701321b18c4ed03309c88ce8ee2fa702
Maroders <DShishlov@gmail.com>
2022-02-19 20:02:09 +0300

    b2 done

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -32,5 +32,19 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+    def get_item(n):
+        return ((-1) ** (n - 1) * x ** (2 * n - 1)) / math.factorial(2 * n - 1)
+
+    sum_ = 0
+    for i in count(1, 1):
+        current_item = get_item(i)
+        sum_ += current_item
+        if abs(current_item) < EPSILON:
+            return sum_
+
+
+
+
+
+print(ex(1.55433))
+print(sinx(1.55433))
