c0f3521469de545a18607292cc84b187a5575198
Maroders <DShishlov@gmail.com>
2022-03-02 17:45:50 +0300

    d1_horse self done

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -10,7 +10,6 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     """
     @lru_cache
     def get_steps(i, j):
-
         row = shape[0]
         col = shape[1]
 
@@ -23,20 +22,39 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
         if not 0 <= j < col:
             return 0
 
-        return sum([get_steps(i - 2, j + 1),
-                    get_steps(i - 2, j - 1),
-                    get_steps(i - 1, j - 2),
-                    get_steps(i + 1, j - 2)
-                    ])
-
+        return sum([get_steps(i - 2, j + 1), get_steps(i - 2, j - 1), get_steps(i - 1, j - 2), get_steps(i + 1, j - 2)])
     return get_steps(point[0], point[1])
-
-
-
-
 
 
 if __name__ == '__main__':
     assert 13309 == calculate_paths((7, 15), (6, 14))
     assert 2 == calculate_paths((4, 4), (3, 3))
+
+    #     row = shape[0]
+    #     col = shape[1]
+    #
+    #     if i == 0 and j == 0:
+    #         return 1
+    #
+    #     if not 0 <= i < row:
+    #         return 0
+    #
+    #     if not 0 <= j < col:
+    #         return 0
+    #
+    #     return sum([get_steps(i - 2, j + 1),
+    #                 get_steps(i - 2, j - 1),
+    #                 get_steps(i - 1, j - 2),
+    #                 get_steps(i + 1, j - 2)
+    #                 ])
+    #
+    # return get_steps(point[0], point[1])
+
+#
+# if __name__ == '__main__':
+#     assert 13309 == calculate_paths((7, 15), (6, 14))
+#     assert 2 == calculate_paths((4, 4), (3, 3))
+
+
+
 
